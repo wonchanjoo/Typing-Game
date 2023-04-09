9197ec3dbbc508e368e23388e98503e5e31ec9be
wonchanjoo <wcj0911@naver.com>
2023-04-09 20:13:57 +0900

    add ScoreWriter.java

---
+++ src/game_io/ScoreWriter.java
@@ -0,0 +1,22 @@
+package game_io;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SourceWriter {
+    private PrintWriter printWriter = null;
+
+    public void scoreWrite(String name, int score) {
+        String text = name + " " + Integer.toString(score);
+        try {
+            printWriter = new PrintWriter(new BufferedWriter(new FileWriter("score.txt", true)));
+            printWriter.println(text);
+            printWriter.flush();
+            printWriter.close();
+        } catch (IOException e) {
+            return;
+        }
+    }
+}
